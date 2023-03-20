ff4fde35158ba1c8518fbad3b6114708d49fe80a
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-20 12:54:04 +0300

    max2 function added

--- function_trial_1/main.c
+++ function_trial_1/main.c
@@ -2,10 +2,19 @@
 
 #include <stdio.h>
 
-int get_abs(int val)
+int get_abs(int val) //about absolute value
 {
     if (val >= 0)
         return val;
 
     return -val;
+}
+
+int max2(int a, int b) //about max value
+{
+    if (a>b)
+        return a;
+
+    return b;
+
 }
