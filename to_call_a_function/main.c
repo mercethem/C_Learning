869c217e970b34713207fe85628dcfb4277636ee
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-20 18:33:18 +0300

    main ver.2 added

--- to_call_a_function/main.c
+++ to_call_a_function/main.c
@@ -8,7 +8,12 @@ int sum2(int a, int b)
 }
 int main(void)
 {
+    printf("Result : %d" , sum2(5,2)); //sum2(5,2) used for to call a function.
+    return 0;
+}
+/**int main(void) //this main function is ver.2
+{
     int result = sum2(5, 2); //sum2(5,2) used for to call a function.
     printf("Result : %d" , result);
     return 0;
-}
+}**/
