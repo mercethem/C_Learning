7bfef8e67a50c6eff5a3bcbd4304e7623aa0ed3d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-24 16:27:09 +0300

    Initial commit

---
+++ scanf/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+    printf("Please enter three numbers : ");
+    scanf("%d%d%d",&x,&y,&z);
+    printf("%d + %d + %d = %d\n",x ,y ,z , x + y + z);
+
+    return 0;
+}
