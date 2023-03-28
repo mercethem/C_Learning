ad792f8fb95d0f9034c7d215dd0ff203ff708b2d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-28 19:31:17 +0300

    initial commit

---
+++ control_statements/main.c
@@ -0,0 +1,24 @@
+
+/**
+
+ if statement
+
+ while statement         switch statement        return statement
+ do while statement      goto statement          break statement
+ for statement                                   continue statement
+--------------------------------------------------------------------
+ if statement
+
+ Uses for conditions
+ if (conditional expression[NOT STATEMENT])  //If conditional expression is TRUE(non-zero) else FALSE(zero)
+    true (body) statement;
+
+ ---> if (expr){
+         true statement;
+      }else{
+         false statement;
+      }
+ You can not use void type!!!
+
+ */
+
