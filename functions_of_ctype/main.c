af8618798c9e0a1bf6b01112721a523a54b04e4b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-29 16:51:58 +0300

    initial commit

---
+++ functions_of_ctype/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h> //Func type int, names start is... int isxxxx(int c); Zero = FALSE, Non-zero = TRUE.
+/**
+ int isupper(int c); //uppercase ?
+ int islower(int c); //lowercase ?
+ int isalpha(int c); //alphabet char?
+ int isdigit(int c); //number char?
+ int isalnum(int c); //alphanumeric char?
+ int isxdigit(int c); //hexadecimal alphanumeric char?
+ int isspace(int c); //whitespace char? ' ', '\n', '\t', '\v', '\f', '\r'
+ int ispunct(int c); //visible or not alphanumeric (% . ; ' ! etc. )
+ int isprint(int c); // have got ' '
+ int isgraph(int c); //  have not got ' '
+ int isblank(int c); // ' ' or '\t' ?
+ int iscntrl(int c); // (invisible char ? ASCII 1, 2 ,3, ..., 32 and 127)
+
+ */
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
