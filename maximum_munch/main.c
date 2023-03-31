6c1dfc47afb981ab92218ed7bc657c0fe27e1602
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-31 13:49:19 +0300

    initial commit

---
+++ maximum_munch/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+
+ Maximum munch rule is about tokenizing.
+
+ while ( n --> 0) {
+    printf("expr.\n");      ---> That is joke!
+ }
+
+ */
+
+int main(void) {
+    int x = 5;
+    int y = 9;
+    int z = x+++y;
+
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("z = %d\n", z);
+    return 0;
+}
