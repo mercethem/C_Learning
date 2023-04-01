0caa4ae99b5221a1bb41d12815d76b6079ef2e23
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-01 21:22:22 +0300

    reformat

--- getch/main.c
+++ getch/main.c
@@ -3,12 +3,14 @@
 
 //_getch() and _getche() don't use same buffer with scanf()
 
-int main() {
+int main()
+{
     int ch;
     printf("Please enter a character : \n");
     ch = _getch(); //Not standard and not echo, not line-buffered.
-    printf("\nch = %d\n",ch);
+    printf("\nch = %d\n", ch);
     ch = _getche(); //Not standard and with echo, not line-buffered.
-    printf("\nch = %d\n",ch);
+    printf("\nch = %d\n", ch);
+
     return 0;
 }
