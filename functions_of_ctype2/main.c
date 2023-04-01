9ddfc34b9322d2e00f321de74113a4037c8dd98d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-01 21:21:47 +0300

    reformat

--- functions_of_ctype2/main.c
+++ functions_of_ctype2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+
 /**
  int isupper(int c); //uppercase ?
  int islower(int c); //lowercase ?
@@ -16,89 +17,90 @@
  int tolower(int c); //uppercase to lowercase
  int toupper(int c); //lowercase to uppercase
  */
-int main() {
+int main()
+{
 
     printf("isupper\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isupper(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("islower\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (islower(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isalpha\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isalpha(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isdigit\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isdigit(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isalnum\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isalnum(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isxdigit\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isxdigit(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isspace\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isspace(i))
-            printf("%d ",i);
+            printf("%d ", i);
     }
 
     printf("\n\n");
     printf("ispunct\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (ispunct(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isprint\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isprint(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isgraph\n"); //Without ' '
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isgraph(i))
             putchar(i);
     }
 
     printf("\n\n");
     printf("isblank\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (isblank(i))
-            printf("%d ",i);
+            printf("%d ", i);
     }
 
     printf("\n\n");
     printf("iscntrl\n");
-    for (int i = 0; i < 128 ; ++i) {
+    for (int i = 0; i < 128; ++i) {
         if (iscntrl(i))
-            printf("%d ",i);
+            printf("%d ", i);
     }
 
     return 0;
