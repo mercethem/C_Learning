17b2c02dfbb882f5607ffe1e5639610c6a4ead98
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-02 16:37:24 +0300

    initial commit

---
+++ factorial_function/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial (int n)
+{
+    int result = 1;
+
+    for (int i = 1; i <= n; ++i)
+        result *= i;
+
+    return result;
+}
+
+int main()
+{
+    int ival;
+
+    printf("Please enter a integer for factorial calculate :");
+    scanf("%d", &ival);
+    printf("%d", factorial(ival));
+
+    return 0;
+}
