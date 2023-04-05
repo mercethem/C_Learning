d5357aa6bea236f49270b05e423e47e47b2ef840
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-05 14:52:18 +0300

    initial commit

---
+++ preprocessor_directives/main.c
@@ -0,0 +1,21 @@
+
+/**
+preprocessor directives
+
+ <...> ---> For standard headers. Preprocessor searches default directory
+ "..." ---> Our creates or 3th library headers. Preprocessor searches source directory
+
+ # (null directive)
+ #include
+ #define
+ #undef
+ #if
+ #else
+ #elif
+ #endif
+ #ifdef
+ #ifndef
+ #line
+ #error
+ #pragma
+*/
