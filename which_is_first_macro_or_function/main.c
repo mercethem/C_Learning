0540b8318ad3c7b6a9f338c1c6d5cb8084a7849d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-06 19:48:05 +0300

    documentation added

--- which_is_first_macro_or_function/main.c
+++ which_is_first_macro_or_function/main.c
@@ -23,5 +23,33 @@ int main() {
 
     return 0;
 
+/**
 
+#define         SQUARE(a)           ((a) * (a))
+
+#include <stdio.h>
+
+int (square)(int x)         // ---> Use like that ---> (square) <--- if macro upper then function
+
+{
+    return x * x;
+}
+
+int main() {
+
+    int ival;
+
+    printf("Please enter a integer :\n");
+    scanf("%d",&ival);
+
+    int y = square(ival);
+    //int y = (square)(ival);   Function uses first!!! Not MACRO because (square) used!
+
+    printf("%d",y);
+
+    return 0;
+
+}
+
+ */
 }
