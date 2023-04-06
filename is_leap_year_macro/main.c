21a8c83aa8266e12938eca8d7ee01bd6a041a985
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-06 18:40:35 +0300

    initial commit

---
+++ is_leap_year_macro/main.c
@@ -0,0 +1,18 @@
+#define         ISLEAP(y)           ((((y) % 4 == 0) && (((y) % 100 != 0) || ((y) % 400 == 0))))
+
+#include <stdio.h>
+
+int main() {
+
+    int year;
+
+    printf("Please enter a year :\n");
+    scanf("%d",&year);
+
+    if (ISLEAP(year))
+        printf("%d is leap year\n",year);
+    else
+        printf("%d is not leap year\n",year);
+
+    return 0;
+}
