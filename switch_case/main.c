e5839b0711bf873f0ea53440a78a01d23872da48
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-17 15:18:52 +0300

    initial commit

---
+++ switch_case/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int day;
+
+    printf("Please enter a day (1-7) :");
+    scanf("%d", &day);
+
+    switch (day) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            printf("Midweek");break;
+        case 6:
+        case 7:
+            printf("Weekend");break;
+        default:
+            printf("Wrong enter, please enter integer between 1 to 7!!!");
+    }
+    return 0;
+}
