b642c625a23d570a6304ce41fa5c138dcd32815b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-18 13:08:13 +0300

    initial commit

---
+++ day_of_year/mylibrary.h
@@ -0,0 +1,11 @@
+//
+// Created by User on 18.04.2023.
+//
+
+#ifndef DAY_OF_YEAR_MYLIBRARY_H
+#define DAY_OF_YEAR_MYLIBRARY_H
+
+#define     isleap(y)       ((y) % 4 == 0 &&  ((y) % 100 != 0 || (y) % 400 == 0))
+
+#endif //DAY_OF_YEAR_MYLIBRARY_H
+
