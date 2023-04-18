526eda53ec96a2968f5d28141b5762295fed8510
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-19 00:20:06 +0300

    initial commit

---
+++ type_conversions/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+    implicit type conversion                        ---> compiler do that automatically and we don't see that
+    ival = dval; First step is dval changes type int, second step is copy dval to ival and this situation transient
+        so temporary!
+
+
+    explicit type conversion (type-cast operator)   --->
+
+
+
+ */
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
