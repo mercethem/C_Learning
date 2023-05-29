b7399583d114cc1e1be17694464a0d46bfc12dd4
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-05-29 07:59:03 +0300

    random function's declarations added

--- mylibrary/mylibrary.h
+++ mylibrary/mylibrary.h
@@ -5,12 +5,17 @@
 #ifndef MYLIBRARY_MYLIBRARY_H
 #define MYLIBRARY_MYLIBRARY_H
 
+#define     RAND_MAX
+
 #define     isleap(y)       ((y) % 4 == 0 && ((y) % 400 == 0 || (y) % 100 != 0))
+
 
 
 int         isprime (int val);
 void        pline(void);
 int         ndigit(int);
+int         rand(void); //Seed value is 1
+void        srand(unsigned int); //Changes the seed value of rand function
 
 
 #endif //MYLIBRARY_MYLIBRARY_H
