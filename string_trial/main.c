39b0563a71a7c8eca34aa5c50b162da0bb35400a
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-07-01 07:46:02 +0300

    reformat

--- string_trial/main.c
+++ string_trial/main.c
@@ -7,16 +7,16 @@ int main()
     char str[SIZE];
 
     printf("Please enter a string\n");
-    scanf("%s",str);
+    scanf("%s", str);
 
-    printf("[%s]\n",str);
+    printf("[%s]\n", str);
     int i;
-    for (i = 0; str[i] != '\0' ; ++i)
+    for (i = 0; str[i] != '\0'; ++i)
         ; //null statemen
 
     str[i] = '!';
     str[i + 1] = '\0';
-    printf("[%s]\n",str);
+    printf("[%s]\n", str);
 
     return 0;
 }
