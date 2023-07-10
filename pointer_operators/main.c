bab3663f190f08069b80a3ba88e228917f01af28
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-07-11 01:31:04 +0300

    initial commit

---
+++ pointer_operators/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/***
+    -------------------
+    1-  [ ]         ->
+    -------------------
+    2-  &           *
+    -------------------
+
+    NAMES:
+
+    [ ] (subscript/index operator)
+    ->  (member selection operator / arrow operator(popular name))
+    &   (address of operator)
+    *   (dereferencing / indirection)
+
+
+ */
+
+int main()
+{
+    printf("Hello Pointers!\n");
+    return 0;
+}
