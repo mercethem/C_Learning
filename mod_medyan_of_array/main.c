dcbc405a3049331e37a8321230b0138baebccfce
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-07-18 07:47:46 +0300

    documentation changed and updated

--- mod_medyan_of_array/main.c
+++ mod_medyan_of_array/main.c
@@ -11,17 +11,13 @@ int main()
 
     set_random_array(a, SIZE);
 
-    for (int i = 0; i < SIZE; ++i)
-    {
+    for (int i = 0; i < SIZE; ++i) {
         printf("%d ", a[i]);
     }
 
-    for (int i = 0; i < SIZE; ++i)
-    {
-        for (int j = 0; j < SIZE - 1; ++j)
-        {
-            if (a[j] > a[j + 1])
-            {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE - 1; ++j) {
+            if (a[j] > a[j + 1]) {
                 temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
@@ -32,8 +28,7 @@ int main()
     putline();
 
     int count = 0;
-    for (int i = 0; i < SIZE; ++i)
-    {
+    for (int i = 0; i < SIZE; ++i) {
         printf("%d ", a[i]);
         ++count;
     }
@@ -42,13 +37,30 @@ int main()
 
     int medyan;
 
-    if ((count % 2) == 0)
-    {
+    if ((count % 2) == 0) {
         medyan = (a[(count / 2) - 1] + a[(count / 2)]) / 2;
     }
-    else{
+    else {
         medyan = a[(count - 1) / 2 + 1];
     }
-    printf("%d", medyan);
+    int mod;
+
+
+    int b[SIZE];
+    int counter = 0;
+    int x;
+
+    for (int i = 0; i < SIZE; ++i) {
+        x = a[i];
+        if (x == a[i + 1]) {
+            ++counter;
+            b[i] = x;
+        }
+    }
+
+
+    printf("median is %d\n", medyan);
+    printf("mod is %d\n", mod);
+
     return 0;
 }
