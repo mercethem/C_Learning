f66aab2bfc2bbe77b33fb9a4f8e41a0bbac45660
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-07-18 07:57:16 +0300

    initial commit

---
+++ swap_function/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int swap(int *ival, int *ival2)
+{
+    int temp = *ival;
+    *ival = *ival2;
+    *ival2 = temp;
+}
+int main()
+{
+    int ival;
+    int ival2;
+
+    printf("Please enter 2 integer:\n");
+    scanf("%d%d", &ival, &ival2);
+
+    printf("1. number is %d\n2. number is %d\n", ival, ival2);
+
+    swap(&ival,&ival2);
+    printf("1. number is %d\n2. number is %d\n", ival, ival2);
+
+    return 0;
+}
