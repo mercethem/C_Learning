1373574c4c0b22b660be58dd860e2c8a09a6a2ad
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-07-23 00:12:45 +0300

    reformat

--- array_control/main.c
+++ array_control/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "mylibrary.h"
 
+
 #define     SIZE    1000
+
 
 int main()
 {
@@ -14,19 +16,15 @@ int main()
     print_array(b, SIZE);
 
 
-    for (int i = 0; i < SIZE; ++i)
-    {
-        for (int j = 0; j < SIZE - 1; ++j)
-        {
-            if (a[j] > a[j + 1])
-            {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE - 1; ++j) {
+            if (a[j] > a[j + 1]) {
                 int temp_a = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp_a;
             }
 
-            if (b[j] > b[j + 1])
-            {
+            if (b[j] > b[j + 1]) {
                 int temp_b = b[j];
                 b[j] = b[j + 1];
                 b[j + 1] = temp_b;
@@ -40,25 +38,20 @@ int main()
     int count = 0;
     int c[SIZE];
 
-    for (int i = 0; i < SIZE; ++i)
-    {
-        if (a[i] == b[i])
-        {
+    for (int i = 0; i < SIZE; ++i) {
+        if (a[i] == b[i]) {
             c[count] = a[i];
             ++count;
         }
     }
 
     int counter = 0;
-    for (int k = 0; k < count; ++k)
-    {
-        if (c[k] != c[k + 1])
-        {
+    for (int k = 0; k < count; ++k) {
+        if (c[k] != c[k + 1]) {
             printf("%d ", c[k]);
             ++counter;
         }
     }
-
 
     printf("\n%d", counter);
 
