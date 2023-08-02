8f2f4407678184af774999aba0dcf5f5bd2795a7
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-02 06:11:07 +0300

    documentation updated

--- return_pointer/main.c
+++ return_pointer/main.c
@@ -3,7 +3,13 @@
  (Address of stack memory associated with local variable 'x' returned)
 
  Can be usable
- 1- Static pointers!(global variable, static local variables, string literals)
+ 1- Static pointers!
+    a-global variable
+    b-static local variables
+    c-string literals
+ 2- Pointer of call by reference
+ 3- Pointer of dynamic life subject
+
  */
 
 
