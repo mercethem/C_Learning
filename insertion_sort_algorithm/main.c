42f91333400dda97989c6e1ab5990c33d37cd0cc
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-02 16:19:36 +0300

    algorithm fixed and for changed to while

--- insertion_sort_algorithm/main.c
+++ insertion_sort_algorithm/main.c
@@ -23,9 +23,10 @@ void insertion_sort(int arr[], int size)
 
     for (int i = 1; i < size; ++i) {
         int key = arr[i];
-        int j;
-        for (j = i - 1; j >= 0 && arr[j] > key; --j) {
+        int j = i - 1;
+        while(arr[j] > key && j >=0){
             arr[j + 1] = arr[j];
+            j--;
         }
         arr[j + 1] = key;
     }
