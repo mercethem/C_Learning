ce5f5ac5531458e002f091ac381a1ee0686817df
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-03 17:20:11 +0300

    initial commit

--- quick_sort/main.c
+++ quick_sort/main.c
@@ -12,7 +12,6 @@
  */
 
 
-
 #include "mylibrary.h"
 
 
@@ -21,13 +20,13 @@
 
 int partition(int arr[], int begin, int end)
 {
-    int pivot = arr[begin]; // pivot
-    int i = (begin - 1); // Index of smaller element
+    int pivot;
+    pivot = arr[begin]; // pivot
+    int i = (begin - 1);
 
     for (int j = begin; j <= end - 1; j++) {
-        // If current element is smaller than the pivot
         if (arr[j] < pivot) {
-            i++; // increment index of smaller element
+            i++;
             swap(&arr[i], &arr[j]);
         }
     }
@@ -35,50 +34,17 @@ int partition(int arr[], int begin, int end)
     return (i + 1);
 }
 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quick_sort(int arr[], int low, int high)
+void quick_sort(int arr[], int begin_idx, int end_idx)
 {
-    if (low < high) {
-        /* part_idx is partitioning index, arr[p] is now
-        at right place */
-        int part_idx = partition(arr, low, high);
+    if (begin_idx < end_idx) {
+        int part_idx = partition(arr, begin_idx, end_idx);
 
-        // Separately sort elements before
-        // partition and after partition
-        quick_sort(arr, low, part_idx - 1);
-        quick_sort(arr, part_idx + 1, high);
+        quick_sort(arr, begin_idx, part_idx - 1);
+        quick_sort(arr, part_idx + 1, end_idx);
     }
 }
 
 
-void quick_sort2(int arr[], int start, int finish)
-{
-    int i = start;
-    int j = finish;
-    int pivot = arr[i];
-
-    do {
-        while (arr[i] < pivot)
-            i++;
-        while (arr[j] > pivot)
-            j--;
-        if (i <= j) {
-            swap(&arr[i], &arr[j]);
-            i++;
-            j--;
-        }
-    }
-    while (i < j);
-    if (i < finish)
-        quick_sort2(arr, i, finish);
-    if (start < j)
-        quick_sort2(arr, start, j);
-}
-
-// Driver program to test above functions
 int main()
 {
     int a[SIZE];
