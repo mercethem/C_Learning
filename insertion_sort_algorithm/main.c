90b198d48a6dc29a274848d332972c572a82747a
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-03 12:50:04 +0300

    reformat and checked

--- insertion_sort_algorithm/main.c
+++ insertion_sort_algorithm/main.c
@@ -24,7 +24,7 @@ void insertion_sort(int arr[], int size)
     for (int i = 1; i < size; ++i) {
         int key = arr[i];
         int j = i - 1;
-        while(arr[j] > key && j >=0){
+        while (arr[j] > key && j >= 0) {
             arr[j + 1] = arr[j];
             j--;
         }
