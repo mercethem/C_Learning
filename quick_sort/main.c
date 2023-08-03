2fe7cb28f58ef3ba2368eb0862ba55fb18d68f94
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-03 18:01:50 +0300

    algorithm fixed

--- quick_sort/main.c
+++ quick_sort/main.c
@@ -20,30 +20,32 @@
 
 int partition(int arr[], int begin, int end)
 {
-    int pivot;
-    pivot = arr[begin]; // pivot
+    int pivot = arr[end];
     int i = (begin - 1);
 
-    for (int j = begin; j <= end - 1; j++) {
-        if (arr[j] < pivot) {
+    for (int j = begin; j <= end- 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
             i++;
             swap(&arr[i], &arr[j]);
         }
     }
     swap(&arr[i + 1], &arr[end]);
+
     return (i + 1);
 }
 
-void quick_sort(int arr[], int begin_idx, int end_idx)
+void quick_sort(int arr[], int low, int high)
 {
-    if (begin_idx < end_idx) {
-        int part_idx = partition(arr, begin_idx, end_idx);
+    if (low < high)
+    {
+        int part_idx = partition(arr, low, high);
 
-        quick_sort(arr, begin_idx, part_idx - 1);
-        quick_sort(arr, part_idx + 1, end_idx);
+        quick_sort(arr, low, part_idx - 1);
+        quick_sort(arr, part_idx + 1, high);
     }
 }
-
 
 int main()
 {
@@ -54,7 +56,6 @@ int main()
     print_array(a, SIZE);
 
     quick_sort(a, 0, SIZE - 1);
-    //quick_sort2(a, 0, SIZE - 1);
     print_array(a, SIZE);
 
     return 0;
