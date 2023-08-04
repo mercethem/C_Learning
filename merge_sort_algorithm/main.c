6b98c3ef4d7280917ce416dfba90ebdd32d1bbcf
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-04 19:27:34 +0300

    documentation and comments checked and someone deleted

--- merge_sort_algorithm/main.c
+++ merge_sort_algorithm/main.c
@@ -23,9 +23,6 @@
 #define     SIZE    10
 
 
-// Merges two sub-arrays of arr[].
-// First subarray is arr[begin..middle]
-// Second subarray is arr[middle+1..end]
 void merge(int arr[], int begin, int middle, int end)
 {
     int i;
@@ -35,22 +32,15 @@ void merge(int arr[], int begin, int middle, int end)
     int idx_1 = middle - begin + 1;
     int idx_2 = end - middle;
 
-    // Create temp arrays
     int L[idx_1], R[idx_2];
 
-    // Copy data to temp arrays L[] and R[]
     for (i = 0; i < idx_1; i++)
         L[i] = arr[begin + i];
     for (j = 0; j < idx_2; j++)
         R[j] = arr[middle + 1 + j];
 
-    // Merge the temp arrays back into arr[begin..end] Initial index of first subarray
     i = 0;
-
-    // Initial index of second subarray
     j = 0;
-
-    // Initial index of merged subarray
     k = begin;
     while (i < idx_1 && j < idx_2) {
         if (L[i] <= R[j]) {
@@ -63,15 +53,11 @@ void merge(int arr[], int begin, int middle, int end)
         }
         k++;
     }
-
-    // Copy the remaining elements of L[], if there are any
     while (i < idx_1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-
-    // Copy the remaining elements of R[], if there are any
     while (j < idx_2) {
         arr[k] = R[j];
         j++;
@@ -79,14 +65,11 @@ void merge(int arr[], int begin, int middle, int end)
     }
 }
 
-// left_idx is for left index and right_idx is right index of the sub-array of arr to be sorted
 void mergeSort(int arr[], int left_idx, int right_idx)
 {
     if (left_idx < right_idx) {
-        // Same as (left_idx+right_idx)/2, but avoids overflow for large left_idx and h
         int middle_idx = left_idx + (right_idx - left_idx) / 2;
 
-        // Sort first and second halves
         mergeSort(arr, left_idx, middle_idx);
         mergeSort(arr, middle_idx + 1, right_idx);
 
