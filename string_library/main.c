98261cc29a395a095fb00bfd897b25376f0a4c39
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-06 14:25:09 +0300

    initial commit

---
+++ string_library/main.c
@@ -0,0 +1,10 @@
+/**
+ String library have standard functions about String
+
+ strlen(string length), strchr, strrchr, strstr, strpbrk, strspn, strcspn, strcpy(string copy), strvat,
+ strcmp(string compare), strncpy, strncmp, strncat, strtok(string tokenizing) etc.
+
+ unless a variable is char* or const char* but do not use length of string
+
+
+ */
