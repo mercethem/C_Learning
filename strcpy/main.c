7febc64f8bcc29d0cd58c2c26e3c726975e2ae2c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-07 07:43:43 +0300

    documentation updated

--- strcpy/main.c
+++ strcpy/main.c
@@ -1,7 +1,18 @@
-// char* strcpy(char* pdest, const char* psource); returns pointer of pdest!
 
-//ATTENTION:If my string is so long then stackoverflow!
-//So dest array size must be enough long to like source array size! (UNDEFINED BEHAVIOR)
+/**
+ char* strcpy(char* pdest, const char* psource); returns pointer of pdest! Until C99
+ char* strcpy(char* restrict pdest, const char* restrict psource);  Since C99
+    (restrick keyword is guaranteed from programmer about for unused overlapped blocks)!!!
+
+
+ ATTENTION:If my string is so long then stackoverflow!
+ So dest array size must be enough long to like source array size! (UNDEFINED BEHAVIOR)
+
+ If you want to use this right way like this situation you have use memmove function!!!
+
+ strcpy(str + 2, str); UNDEFINED BEHAVIOR!!! because os overlapped blocks!!!
+
+ */
 
 #include <stdio.h>
 #include "mylibrary.h"
