d2825932f938bc97926ce92c923945feda33e341
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-07 06:12:37 +0300

    mystrrchr function added

--- strchr/main.c
+++ strchr/main.c
@@ -10,19 +10,35 @@
 
 #define     SIZE    100
 
+char* mystrrchr(const char* p, int c)
+{
+    const char* pfound = NULL;
+
+    while (*p) {
+        if (*p == c)
+            pfound = p;
+        ++p;
+    }
+    if (!c)
+        return (char*)p;
+    return (char*)pfound;
+}
+
 
 int main()
 {
     char s[SIZE];
     char* p;
+    char* p2;
 
     printf("Please enter a string:");
     sgets(s);
     p = strchr(s, 'a');
+    p2 = mystrrchr(s, 'a');
 
-    if (p != NULL) {
-        printf("Found your character and index is %zu\n", p - s);
-        *p = '*';
+    if (p2 != NULL) {
+        printf("Found your character and index is %zu\n", p2 - s);
+        *p2 = '*';
         puts(s);
     }
     else {
