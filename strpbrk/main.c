41e90b9b73f399093b163d54c44e8b4034de9152
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-07 06:50:47 +0300

    initial commit

---
+++ strpbrk/main.c
@@ -0,0 +1,30 @@
+//char* strpbrk(const char* psource, const char* pchars);
+
+#include <stdio.h>
+#include "mylibrary.h"
+#include <string.h>
+
+#define     SIZE    100
+
+int main()
+{
+    char str[SIZE];
+    char s[] = "aeou";
+
+    printf("Please enter a string:\n");
+    sgets(str);
+
+    char* p = strpbrk(str, s);
+
+    if (p) {
+        printf("Found! Index is %lld\n", p - str);
+        *p = '!';
+        puts(str);
+
+
+    }
+    else {
+        printf("UNFOUNDED!");
+    }
+    return 0;
+}
