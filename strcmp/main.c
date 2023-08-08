7dc0ed7ea3aa2466f462eba9850022baf9cc9258
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-08 06:50:00 +0300

    mystrcmp function added and test function added

--- strcmp/main.c
+++ strcmp/main.c
@@ -41,8 +41,43 @@
 #include <string.h>
 
 
+#define     SIZE    100
+
+int mystrcmp(const char* p1, const char* p2)
+{
+    while (*p1 == *p2)
+    {
+        if(*p1 == '\0')
+            return 0;
+        ++p1;
+        ++p2;
+    }
+
+    return *p1 - *p2;
+}
+
+
 int main()
 {
+    char s1[SIZE];
+    char s2[SIZE];
+    int result;
+
+    printf("Please enter two words:\n");
+    scanf("%s%s", s1, s2);
+
+    result = mystrcmp(s1, s2);
+
+    if (result > 0) {
+        printf("%s > %s\n", s1, s2);
+    }
+    else if (result < 0) {
+        printf("%s > %s\n", s2, s1);
+    }
+    else {
+        printf("%s = %s\n", s1, s2);
+
+    }
 
     return 0;
 }
