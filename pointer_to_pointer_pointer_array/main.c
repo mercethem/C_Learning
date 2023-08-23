83519d7d9d55ac0e002e7528413fbfc36f0104f7
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-23 19:05:43 +0300

    reformat

--- pointer_to_pointer_pointer_array/main.c
+++ pointer_to_pointer_pointer_array/main.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include "mylibrary.h"
 
-void sort_names(char** pa, size_t size){
-    for(size_t i = 0; i < size;++i){
-        for (size_t k = 0; k < size - 1- i; ++k) {
-            if(strcmp(pa[k], pa[k +1]) > 0){
+void sort_names(char** pa, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t k = 0; k < size - 1 - i; ++k) {
+            if (strcmp(pa[k], pa[k + 1]) > 0) {
                 char* ptemp = pa[k];
-                pa[k] = pa[k + 1 ];
+                pa[k] = pa[k + 1];
                 pa[k + 1] = ptemp;
             }
         }
@@ -16,7 +17,7 @@ void sort_names(char** pa, size_t size){
 
 void print_names(char** pa, size_t size)
 {
-    for(size_t i = 0; i < size; ++i)
+    for (size_t i = 0; i < size; ++i)
         printf("%s ", pa[i]);
 
     printf("\n");
@@ -24,14 +25,14 @@ void print_names(char** pa, size_t size)
 
 void print_names2(char** pa, size_t size)
 {
-    while(size--)
+    while (size--)
         printf("%s", *pa++);
     printf("\n");
 }
 
 int main()
 {
-    char* p[] = {"george", "michael", "helen", "eve" };
+    char* p[] = {"george", "michael", "helen", "eve"};
 
     print_names(p, asize(p));
     print_names2(p, asize(p));
