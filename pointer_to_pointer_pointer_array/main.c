220bf6dd05651f00725a15ac684f11466ed8f6fd
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-23 20:15:22 +0300

    equal functions added

--- pointer_to_pointer_pointer_array/main.c
+++ pointer_to_pointer_pointer_array/main.c
@@ -2,6 +2,26 @@
 #include <string.h>
 #include "mylibrary.h"
 
+
+void pcswap(char** p1, char** p2)
+{
+    char* ptemp = *p1;
+    *p1 = *p2;
+    *p2 = ptemp;
+
+}
+
+void sort_names2(char** pa, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t k = 0; k < size - 1 - i; ++k) {
+            if (strcmp(pa[k], pa[k + 1]) > 0) {
+                pcswap(pa + k, pa + k + 1);
+            }
+        }
+    }
+}
+
 void sort_names(char** pa, size_t size)
 {
     for (size_t i = 0; i < size; ++i) {
@@ -26,7 +46,7 @@ void print_names(char** pa, size_t size)
 void print_names2(char** pa, size_t size)
 {
     while (size--)
-        printf("%s", *pa++);
+        printf("%s ", *pa++);
     printf("\n");
 }
 
@@ -37,6 +57,8 @@ int main()
     print_names(p, asize(p));
     print_names2(p, asize(p));
     sort_names(p, asize(p));
+    print_names2(p, asize(p));
+    sort_names2(p, asize(p));
     print_names2(p, asize(p));
 
     return 0;
