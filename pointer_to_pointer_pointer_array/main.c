3b6c2dd9656e79d353491bb02e5b984ffa6fc6f1
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-23 19:05:34 +0300

    initial commit

--- pointer_to_pointer_pointer_array/main.c
+++ pointer_to_pointer_pointer_array/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include "mylibrary.h"
 
+void sort_names(char** pa, size_t size){
+    for(size_t i = 0; i < size;++i){
+        for (size_t k = 0; k < size - 1- i; ++k) {
+            if(strcmp(pa[k], pa[k +1]) > 0){
+                char* ptemp = pa[k];
+                pa[k] = pa[k + 1 ];
+                pa[k + 1] = ptemp;
+            }
+        }
+    }
+}
 
 void print_names(char** pa, size_t size)
 {
@@ -13,7 +25,7 @@ void print_names(char** pa, size_t size)
 void print_names2(char** pa, size_t size)
 {
     while(size--)
-        printf("%s ", *pa++);
+        printf("%s", *pa++);
     printf("\n");
 }
 
@@ -22,6 +34,8 @@ int main()
     char* p[] = {"george", "michael", "helen", "eve" };
 
     print_names(p, asize(p));
+    print_names2(p, asize(p));
+    sort_names(p, asize(p));
     print_names2(p, asize(p));
 
     return 0;
