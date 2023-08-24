4ff726aaa4af407f671511058bb7ca16d0858220
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-24 21:51:55 +0300

    documentations update for information of generic functions

--- string_library/main.c
+++ string_library/main.c
@@ -4,6 +4,10 @@
  strlen(string length), strchr, strrchr, strstr, strpbrk, strspn, strcspn, strcpy(string copy), strvat,
  strcmp(string compare), strncpy, strncmp, strncat, strtok(string tokenizing) etc.
 
+ Generic Functions of String Library (have got void* and size parameters)
+ -----------------------------------
+ memset, memcpy, memmove, memchr, memcmp
+
  unless a variable is char* or const char* but do not use length of string
 
 
