0d66b175e1def7d5d39b02eb52732ea46cdd3995
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-26 00:54:02 +0300

    initial commit

---
+++ function_pointers/main.c
@@ -0,0 +1,14 @@
+/**
+
+    There is pointer for function!
+
+    How can do that ?
+
+    1)  int func(int);
+        &func; function pointer!!!
+
+    2)  Function to pointer conversion such as array decay (Array to pointer conversion)
+        int func(int);
+        func; function pointer!!!
+
+ */
