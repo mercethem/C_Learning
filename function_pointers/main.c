1b9ab83c55978e4464171664489ddf676da5ccd5
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-08-27 17:03:47 +0300

    mylibrary.c and .h added and some examples about function pointers usage patterns added

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -10,5 +10,41 @@
     2)  Function to pointer conversion such as array decay (Array to pointer conversion)
         int func(int);
         func; function pointer!!!
+    ------------------------------------------------------------------------------------------------
+
+    int func(int);
+    double foo(double, double);
+
+    int main()
+    {
+
+    &func;      ===> type of func is ===> int (*)(int)
+    &foo        ===> type of func is ===> double (*)(double, double)
+    &strcmp     ===> type of func is ===> int (*)(const char*, const char*)
+    &strlen     ===> type of func is ===> size_t (*)(const char*)
+
+    }
 
  */
+
+
+#include <string.h>
+#include "mylibrary.h"
+
+
+int foo(int);
+int func(int);
+
+int main()
+{
+    int (* fp)(int) = &foo; //equal to ===> int (* fp)(int) = &foo;
+
+    fp = &func; //equal to ===> fp = func;
+
+    int (*fpy)(const char*, const char*) = &strcmp;
+    size_t (*fptr)(const char*) = &strlen;
+    void (*fpx)(const int*, size_t) = &print_array;
+
+    return 0;
+
+}
