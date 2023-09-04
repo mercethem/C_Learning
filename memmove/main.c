60873ce743a5a3029fb83da6fddbef93d4e3479b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-04 18:30:52 +0300

    reformat

--- memmove/main.c
+++ memmove/main.c
@@ -7,7 +7,7 @@ int main()
 {
     char str[SIZE] = "congratulations";
 
-    memmove(str + 4 , str, strlen(str) + 1 /* with null '\0' */ );
+    memmove(str + 4, str, strlen(str) + 1 /* with null '\0' */ );
     //ATTENTION :  NOT UB like memcpy!!! When you use this function ? If there is overlapped!!!
     puts(str);
 
