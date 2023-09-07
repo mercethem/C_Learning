d627cf008024f560c1275792155369e0d51840db
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-07 23:38:07 +0300

    updated (comment, new function and new value added and print)

--- Data_Structures/link_list/main.c
+++ Data_Structures/link_list/main.c
@@ -1,35 +1,74 @@
+/**
+ Linked lists are do sequential access
+ */
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
+
 struct n
 {
-    int x;
+    int data;
     struct n* next;
 };
 
 typedef struct n node;
+
+
+void listprint(node* root)
+{
+    while (root != NULL) {
+        printf("%d ", root -> data);
+        root = root -> next;
+    }
+}
+
 
 int main()
 {
     node* root;
 
     root = (node*)malloc(sizeof(node));
-    root -> x = 10;
+    root -> data = 10;
 
     root -> next = (node*)malloc(sizeof(node));
-    root -> next -> x = 20;
+    root -> next -> data = 20;
 
-    root -> next ->  next = (node*)malloc(sizeof(node));
-    root -> next -> next -> x = 30;
+    root -> next -> next = (node*)malloc(sizeof(node));
+    root -> next -> next -> data = 30;
 
-    node * iter;
+    node* iter;
     iter = root;
 
-    printf("%d\n", iter -> x);
+    printf("%d\n", iter -> data);
 
     iter = iter -> next;
-    printf("%d\n", iter -> x);
+    printf("%d\n", iter -> data);
 
     iter = iter -> next;
-    printf("%d\n", iter -> x);
+    printf("%d\n", iter -> data);
+
+
+    root -> next -> next -> next = NULL;
+    iter = root;
+    int i = 0;
+
+    while (iter -> next) { //use to print all linked list
+        i++;
+        printf("Index %d is %d\n", i, iter -> data);
+        iter = iter -> next;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        iter -> next = (node*)malloc(sizeof(node));
+        iter = iter -> next;
+        iter -> data = i * 10;
+        iter -> next = NULL;
+    }
+
+    listprint(root);
+
+
+    return 0;
 }
