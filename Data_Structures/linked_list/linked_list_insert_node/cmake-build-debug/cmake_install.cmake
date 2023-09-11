fbf953ac0e12ab7871b3671c6d771abbbfc23680
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-11 17:13:57 +0300

    initial commit

--- Data_Structures/linked_list/linked_list_insert_node/cmake-build-debug/cmake_install.cmake
+++ Data_Structures/linked_list/linked_list_insert_node/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/C_Learning/Data_Structures/linked_list_insert_node
+# Install script for directory: E:/C_Learning/Data_Structures/linked_list/linked_list_insert_node
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/C_Learning/Data_Structures/linked_list_insert_node/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/C_Learning/Data_Structures/linked_list/linked_list_insert_node/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Data_Structures/linked_list/linked_list_update_function/cmake-build-debug/cmake_install.cmake
+++ Data_Structures/linked_list/linked_list_update_function/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/C_Learning/Data_Structures/linked_list_update_function
+# Install script for directory: E:/C_Learning/Data_Structures/linked_list/linked_list_update_function
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/C_Learning/Data_Structures/linked_list_update_function/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/C_Learning/Data_Structures/linked_list/linked_list_update_function/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
