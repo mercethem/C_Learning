77dca7a755101bdfad651b8f0aeac5dae4e4f720
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-13 16:55:33 +0300

    initial commit

---
+++ C_Syntax/multi_dimensional_arrays_in_problem2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    double temperature[12][31]; //Temperatures of Berlin in 2020
+    double temperature_all[208 ][12][31]; //Temperatures of all countries in 2020
+
+
+
+    return 0;
+}
