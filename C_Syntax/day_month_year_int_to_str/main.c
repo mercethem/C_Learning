84c59e08edd113637f2b4fd1742cebcffb65df2b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-14 13:22:12 +0300

    reformat

--- C_Syntax/day_month_year_int_to_str/main.c
+++ C_Syntax/day_month_year_int_to_str/main.c
@@ -8,11 +8,11 @@
 int main()
 {
     int ival;
+
     printf("Plase enter a integer:\n");
     scanf("%d", &ival);
 
     char str[SIZE];
-
     int temp = ival;
     int idx = 0;
 
@@ -20,6 +20,7 @@ int main()
         str[idx++] = temp % 10 + '0';
         temp /= 10;
     }
+
     str[idx] = '\0';
 
     _strrev(str);
@@ -30,20 +31,23 @@ int main()
 //    }
     printf("(%d) (%s)\n", ival, str);
 
-
     int ival2;
 
     char str2[SIZE];
+
     printf("Plase enter a integer:\n");
     scanf("%d", &ival2);
 
     /** _itoa(int val, char *buf, int base); */ //atoi, atol, atoll(c99), atof etc.
     _itoa(ival2, str2, 16);
     printf("%s\n", str2);
+
     _itoa(ival2, str2, 10);
     printf("%s\n", str2);
+
     _itoa(ival2, str2, 8);
     printf("%s\n", str2);
+
     _itoa(ival2, str2, 2);
     printf("%s\n", str2);
 
