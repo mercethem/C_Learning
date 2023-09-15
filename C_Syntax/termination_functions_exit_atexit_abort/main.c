c6a23a6ebc6481ef610e18f67fb1f3e1611444d7
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-15 16:57:05 +0300

    initial commit

---
+++ C_Syntax/termination_functions_exit_atexit_abort/main.c
@@ -0,0 +1,19 @@
+/**
+  Normal termination : exit ===> exit(EXIT_SUCCESS) successful / exit(EXIT_FAILURE)
+  Normal termination : atexit
+  Abnormal termination : abort
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define     SIZE    100
+
+
+int main()
+{
+
+    return 0;
+}
