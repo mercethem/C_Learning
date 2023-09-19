a58df985a40981338a1ced86f632cadf53191664
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-19 15:52:22 +0300

    initiial commit

--- C_Syntax/pseudo_matrix_with_pointer/main.c
+++ C_Syntax/pseudo_matrix_with_pointer/main.c
@@ -7,6 +7,7 @@ int main()
 {
     size_t row;
     size_t col;
+
     printf("How much do you want to col:\n");
     scanf("%zu", &col);
     printf("How much do you want to row:\n");
@@ -18,31 +19,34 @@ int main()
         return 1;
     }
 
+    int** pp = (int**)malloc(row * sizeof(int*));
+    if (!pp) {
+        fprintf(stderr, "Out of memory!\n");
+        return 1;
+    }
+
+    for (size_t i = 0; i < row; ++i) {
+        pp[i] = pd + col * i;
+    }
+
     for (size_t i = 0; i < row; ++i) {
         for (size_t j = 0; j < col; ++j) {
-            pd[i * col + j] = rand() % 10;
+            pp[i][j] = rand() % 10;
         }
     }
 
-
     for (size_t i = 0; i < row; ++i) {
         for (size_t j = 0; j < col; ++j) {
-            printf("%d ", pd[i * col + j]);
+            printf("%d ", pp[i][j]);
         }
         printf("\n");
     }
-    printf("------------------------------------------------\n");
-
-
-    int* ptr = pd;
-    size_t n = row * col;
-    while (n--){
-        printf("%d ", *ptr++);
-    }
-
+    printf("---------------------------------------------\n");
 
     memset(pd, 0, row * col * sizeof(int));
+    memset(pp, 0, row * sizeof(int*));
     free(pd);
+    free(pp);
 
     return 0;
 }
