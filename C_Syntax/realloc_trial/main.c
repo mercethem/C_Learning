886dffc05a481a8d6ffd81204a14c59b634a5f18
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-21 00:16:12 +0300

    reformat

--- C_Syntax/realloc_trial/main.c
+++ C_Syntax/realloc_trial/main.c
@@ -15,10 +15,15 @@ int main()
 
     for (;;) {
         printf("Do you want to get a integer (y) (n) :");
-        while ((ch = _getch()) != 'y' && ch != 'n'); //null statement
+
+        while ((ch = _getch()) != 'y' && ch != 'n')
+            ;//null statement
+
         printf("%c\n", ch);
+
         if (ch == 'n')
             break;
+
         printf("Please enter integer:\n");
         ival = rand() % 1000;
         printf("%d\n", ival);
@@ -28,8 +33,9 @@ int main()
             fprintf(stderr, "Out of memory!\n");
             exit(EXIT_FAILURE);
         }
+
         pd[cnt++] = ival;
-        print_array(pd,cnt);
+        print_array(pd, cnt);
         _getch();
         system("cls");
     }
@@ -42,5 +48,6 @@ int main()
 //        print_array(pd, cnt);
 //        free(pd);
 //    }
+
     return 0;
 }
