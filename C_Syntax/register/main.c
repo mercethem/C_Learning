bba4551963b053cd4812e7027eed906fd58adfb6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-24 15:07:26 +0300

    updated documentation

--- C_Syntax/register/main.c
+++ C_Syntax/register/main.c
@@ -1,7 +1,12 @@
 /**
 
+ register is memory in cpu.
  register is a keyword for register memory in CPU.
+ register use for speed but register memory is not bigger than ram!
+ Nowadays have not used much more this keyword because of compiler optimization(modern systems)
+    so register is redundant nowadays!
 
+ register can not use for global variables because of registers have not pointer addresses!
  */
 
 #include <stdio.h>
