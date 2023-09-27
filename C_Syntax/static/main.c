55c994a97e4ac5607eacda33e3b3b9af8b23c802
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-28 01:14:23 +0300

    initial commit

--- C_Syntax/static/main.c
+++ C_Syntax/static/main.c
@@ -1,4 +1,5 @@
 /**
+ *
  static have got two different meaning so global scope meaning and local scope meaning
  global static variable - if you use static in global scope your variable do not lose on progress so static storage duration
  local static variable - if you use static in local scope your variable do
@@ -6,11 +7,11 @@
  that is difference is globals can be usable anywhere in sourcecode but local ones are just can be usable in local scope!
  ATTENTION : Global static variable is much more open to code warning because of hard to test and
             hard to control in all codes!!!
+
  */
 
 #include <stdio.h>
 #include "mylibrary.h"
-
 
 
 void func()
@@ -23,9 +24,9 @@ void func()
         ++x[i];
     }
 }
+
 void func2()
 {
-
     int y[5] = {0};
 
     print_array(y, 5);
@@ -34,14 +35,15 @@ void func2()
         ++y[i];
     }
 }
+
 int main()
 {
     for (int i = 0; i < 10; ++i) {
         func();
     }
-
     for (int i = 0; i < 10; ++i) {
         func2();
     }
+
     return 0;
 }
