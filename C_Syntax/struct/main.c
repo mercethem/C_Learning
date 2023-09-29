a22dd8f336266f2ef1cd1eff95606896f9634ac1
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-30 01:42:06 +0300

    comments updated and struct updated

--- C_Syntax/struct/main.c
+++ C_Syntax/struct/main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 
+struct DataRoot{
+    int a;
+};
 
 struct Data //structure tag
 {
-//structure members
-    int a;
-    int b;
-    double dval;
-    double dval2;
+//structure members     ATTENTION: Compiler do not get any memory in ram for this struct!
+                                    //That is just data not and data no need any memory
+                                    //If you create a member of Data then compiler get a memory!!!
+                                    //for example ===> struct Data x; compiler get memory now!
+                                    //Because struct just a notification!!!!
+    int x;
+    double y;
+    int* p;
+    int a[20];
+    char str[40];
+    int (*pFunction)(int);
+    int b[2][5];
+    struct DataRoot myroot; //nested struct!
+
     //ATTENTION: These are contiguous in memory that style!
+    //ATTENTION: You can not use a function in struct! (Can be in C++)
+    //Member selection operators are ===>  . and  -> (left associative) (dot operator and arrow operator)
+    // dot operator use for struct element himself but arrow operator for pointer of struct element
+    // mydata.x; ptr -> x;
+
 };
 
 static struct Data g;
