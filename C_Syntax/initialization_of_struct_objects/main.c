3a0268b1904a0791a984f8f9e52bc62069cd8afe
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-30 20:04:08 +0300

    new struct added

--- C_Syntax/initialization_of_struct_objects/main.c
+++ C_Syntax/initialization_of_struct_objects/main.c
@@ -3,4 +3,13 @@ struct Data
     int x;
     double a[2];
     int y;
-} g = {1, 2.4, 3.6, 5}; //equal to ===> struct Data g = {1, 2.4, 3.6, 5}
+}k, l, g = {1, 2.4, 3.6, 5}; //equal to ===> struct Data g = {1, 2.4, 3.6, 5}
+
+struct
+{
+    int x;
+    double a[2];
+    int y;
+}t, p; //if you use like that so there is no struct tag, then you have to use just t and p!!!!
+        //You can not create a new struct like struct Data because of that is just struct as result
+        //here is no struct tag!
