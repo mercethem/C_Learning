2c9a21d8d159a6e2ae0c01b7bd73eeaf77464d39
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-30 20:06:24 +0300

    new struct added

--- C_Syntax/initialization_of_struct_objects/main.c
+++ C_Syntax/initialization_of_struct_objects/main.c
@@ -13,3 +13,10 @@ struct
 }t, p; //if you use like that so there is no struct tag, then you have to use just t and p!!!!
         //You can not create a new struct like struct Data because of that is just struct as result
         //here is no struct tag!
+
+struct //that struct different to upper one!!!
+{
+    int x;
+    double a[2];
+    int y;
+}j, h;
