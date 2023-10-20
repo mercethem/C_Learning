0d345c16c2274373440407532d5d76b8d48e9927
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-10-20 15:13:07 +0300

    initial commit

---
+++ C_Syntax/self_referential_structure_for_data_types/main.c
@@ -0,0 +1,8 @@
+struct Data{ //Self referential structure (Generally uses in data structure)
+    int x;
+    struct Data* p;
+    /**
+     ATTENTION: struct Data p; That is illegal!!! That p is not a pointer!
+     */
+};
+
