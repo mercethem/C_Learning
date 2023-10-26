101169218795005ea9de3491b39373ea49bbac86
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-10-26 06:26:24 +0300

    handle(ListHandle) used for in studentlist.h

--- C_Syntax/handle/studentlist.h
+++ C_Syntax/handle/studentlist.h
@@ -6,16 +6,21 @@
 
 #include <stddef.h>
 
+struct List;
 struct Student;
 typedef struct Student Student;
+typedef struct List* ListHandle;
 
-void push_front(const Student* p);
-void pop_front(void);
-void get_first(Student* p);
-void print_list(void);
-size_t get_size(void);
-int is_empty(void);
-void make_empty(void);
+ListHandle create_list(void);
+void destroy_list(ListHandle);
+
+void push_front(ListHandle, const Student* p);
+void pop_front(ListHandle);
+void get_first(ListHandle, Student* p);
+void print_list(ListHandle);
+size_t get_size(ListHandle);
+int is_empty(ListHandle);
+void make_empty(ListHandle);
 
 
 
